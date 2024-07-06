05389f0bed0b6f4befdf67111ec4e391490be962
karthi-githubk <149150214+karthi-githubk@users.noreply.github.com>
2024-07-06 20:33:55 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: ["https://ekart-chi.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+   
     exposedHeaders: ["Content-Length", "Authorization"],
   })
 );
