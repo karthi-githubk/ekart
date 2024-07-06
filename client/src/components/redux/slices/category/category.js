344d52ece5bba2db7515135cdd19859b5f0abbe2
karthi-githubk <149150214+karthi-githubk@users.noreply.github.com>
2024-07-06 20:38:29 +0530

    Update category.js

--- client/src/components/redux/slices/category/category.js
+++ client/src/components/redux/slices/category/category.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const backendAPI = 'http://localhost:5005';
+const backendAPI = 'https://ekart-57l0.onrender.com';
 
 
 const initialState = {
